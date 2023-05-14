0a4fda63717f684c819770991e78a1a2b1e9dfb5
fabricio <cdpaiva24530@gmail.com>
2023-05-14 11:19:12 -0300

    Criação na navbar responsiva

--- src/App.js
+++ src/App.js
@@ -3,10 +3,28 @@ import './App.css';
 
 function App() {
   return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
     <div className="container-fluid">
-     <h1>Projeto Gerador de IDs</h1>
+      <a className="navbar-brand fs-4" href="#">Gerador de ID</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-links" aria-controls="navbar-links" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+  
+      <div className="collapse navbar-collapse" id="navbar-links">
+          <ul className="navbar-nav mx-auto mb-2 mb-md-0 ">
+            <li className="nav-item">
+              <a className="nav-link" href="#">UUID</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">GUID</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">CUID</a>
+            </li>
+          </ul>
+      </div>
     </div>
+  </nav>
   );
 }
-
 export default App;
